d3f57a18bfb7a70860e54d758205fb54cd4e95b7
akkayy <techakayy@gmail.com>
2022-08-10 20:33:15 +1000

    Custom vite plugin

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,16 @@ import Vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [Vuetify({ autoImport: true })],
+	plugins: [
+		{
+			configResolved(config) {
+				const idx_vue = config.plugins.findIndex(
+					(plugin) => plugin.name && plugin.name === 'vite:vue'
+				)
+				config.plugins.splice(idx_vue + 1, 0, Vuetify()[0])
+			}
+		}
+	],
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url))
